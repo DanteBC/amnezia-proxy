b216110fb85b4f3b76379a7ecae863deb7e29320
Ilya Antipin <antipin.i@tecomgroup.ru>
2026-09-23 15:12:17 +0300

    moving from 3proxy to gost for proxying, and adding amneziawg-go to vpn image for wireguard userspace implementation

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -1,44 +1,11 @@
 #!/bin/sh
 set -eu
 
-config=/tmp/3proxy.cfg
 username=${PROXY_USERNAME:?PROXY_USERNAME is required}
 password=${PROXY_PASSWORD:?PROXY_PASSWORD is required}
 whitelist=${PROXY_WHITELIST:-}
 
-print_rules() {
-  set -f
+http_service="http://0.0.0.0:8888"
+socks_service="socks5://0.0.0.0:1080"
 
-  if [ "$whitelist" = '*' ]; then
-    printf 'allow %s * * *\n' "$username"
-    return
-  fi
-
-  if [ -n "$whitelist" ]; then
-    old_ifs=$IFS
-    IFS=,
-    for address in $whitelist; do
-      [ -n "$address" ] && printf 'allow %s * %s *\n' "$username" "$address"
-    done
-    IFS=$old_ifs
-  fi
-}
-
-{
-  printf 'nscache 65536\n'
-  printf 'log\n'
-  printf 'auth strong\n'
-  printf 'users "%s:CL:%s"\n' "$username" "$password"
-
-  print_rules
-  printf 'deny * * * *\n'
-  printf 'proxy -4 -p8888\n'
-
-  printf 'flush\n'
-  printf 'auth strong\n'
-  print_rules
-  printf 'deny * * * *\n'
-  printf 'socks -4 -p1080\n'
-} > "$config"
-
-exec /bin/3proxy "$config"
+exec /bin/gost -DD -L "$http_service" -L "$socks_service"
